13333e7b35ae73de396d90b2976aafded0349aeb
Samuel Bekele <sammabek2020@gmail.com>
2022-08-25 15:32:44 +0300

    Fixed AWS S3 2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const httpServer = createServer(app);
 console.log(process.env.EMAIL_FROM);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://gabaa-ecom.web.app",
     methods: ["GET", "POST", "PATCH"],
     credentials: true,
   },
