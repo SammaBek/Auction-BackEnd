46d4f358378313d483c5719553d8cefeb2e6d150
Samuel Bekele <sammabek2020@gmail.com>
2022-12-14 20:09:26 +0300

    fixed environment variables

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -20,7 +20,7 @@ const MIME_TYPE_MAP = {
 
 aws.config.setPromisesDependency();
 aws.config.update({
-  accessKeyId: AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "us-east-1",
 });
